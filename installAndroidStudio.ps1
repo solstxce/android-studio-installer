1880454d2f3477da84d278f16538fceabc3a0496
solstxce <48374053+solstxce@users.noreply.github.com>
2023-12-24 19:39:25 +0530

    Update installAndroidStudio.ps1

--- installAndroidStudio.ps1
+++ installAndroidStudio.ps1
@@ -7,4 +7,6 @@ scoop bucket add java
 scoop install java/openjdk17
 aria2c -x 16 -s 16 https://1111-releases.cloudflareclient.com/windows/Cloudflare_WARP_Release-x64.msi
 ./Cloudflare_WARP_Release-x64.msi
+Start-Sleep 60
 scoop install android-clt
+scoop install android-sdk

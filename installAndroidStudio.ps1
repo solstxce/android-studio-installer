6579603b3012f86a236bf99b9ec33b359809acb9
solstxce <48374053+solstxce@users.noreply.github.com>
2023-12-24 20:02:37 +0530

    Update installAndroidStudio.ps1

--- installAndroidStudio.ps1
+++ installAndroidStudio.ps1
@@ -1,12 +1,23 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+try {
+    Get-Command -Name "scoop"
+}
+catch {
+    irm https://get.scoop.sh | iex
+}
+try {
+    Get-Command "warp-cli"
+}
+catch {
+    aria2c -x 16 -s 16 https://1111-releases.cloudflareclient.com/windows/Cloudflare_WARP_Release-x64.msi
+    ./Cloudflare_WARP_Release-x64.msi
+    Start-Sleep 60
+}
 scoop install git
 scoop bucket add extras
+scoop install grep
 scoop install android-studio
 scoop bucket add java
 scoop install java/openjdk17
-aria2c -x 16 -s 16 https://1111-releases.cloudflareclient.com/windows/Cloudflare_WARP_Release-x64.msi
-./Cloudflare_WARP_Release-x64.msi
-Start-Sleep 60
 scoop install android-clt
 scoop install android-sdk

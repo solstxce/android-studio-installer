571022a673a00cb291932c6c7fc5962f53e90195
solstxce <48374053+solstxce@users.noreply.github.com>
2024-01-06 18:28:22 +0530

    Ask for KLU-WIFI

--- installAndroidStudio.ps1
+++ installAndroidStudio.ps1
@@ -5,20 +5,29 @@ try {
 catch {
     irm https://get.scoop.sh | iex
 }
-try {
-    Get-Command "warp-cli" -ErrorAction Stop
-}
-catch {
-    aria2c -x 16 -s 16 https://1111-releases.cloudflareclient.com/windows/Cloudflare_WARP_Release-x64.msi
-    ./Cloudflare_WARP_Release-x64.msi
-    Start-Sleep 60
+$userInput = Read-Host "Are you using KLU-WIFI? [y/n] :"
+if ($userInput -eq "y") {
+    Write-Host "We will be installing a VPN now due to KLU-WIFI blocking dl.google.com\n"
+    try {
+        Get-Command "warp-cli" -ErrorAction Stop
+    }
+    catch {
+        aria2c -x 16 -s 16 https://1111-releases.cloudflareclient.com/windows/Cloudflare_WARP_Release-x64.msi
+        ./Cloudflare_WARP_Release-x64.msi
+        while (-not (Get-Command "warp-cli" -ErrorAction Stop)) {
+         Write-Host "Waiting for warp-cli to be installed..."
+         Start-Sleep -Seconds 10
+     }
+    }
 }
 scoop install git
 scoop bucket add extras
 scoop install grep
 scoop install android-studio@2022.3.1.22
 scoop bucket add java
-warp-cli connect
+if ($userInput -eq "y") {
+    warp-cli connect
+}
 scoop install adb
 scoop install java/openjdk17
 scoop install android-clt
